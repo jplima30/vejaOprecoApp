a31724775524832f6308c2ed11397eb0248b23c0
JoaoSilva <jplimadev@gmail.com>
2026-05-03 21:19:18 -0300

    Arquitetura e estruturação do projeto
    #18

---
+++ VejaOpreco/VejaOprecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VejaOprecoApp.swift
+//  VejaOpreco
+//
+//  Created by Joao Paulo Lima Silva on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct VejaOprecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
