24a3063a9c0ee58ccdfdb9e44511d41f47fc22ca
JoaoSilva <jplimadev@gmail.com>
2026-05-05 22:11:29 -0300

    feat: adiciona modelo de dados OfertaItem inicial

---
+++ VejaOpreco/Models/OfertaItem.swift
@@ -0,0 +1,16 @@
+//
+//  OfertaItem.swift
+//  VejaOpreco
+//
+//  Created by Joao Paulo Lima Silva on 05/05/26.
+//
+import Foundation
+
+struct OfertaItem: Codable,Identifiable {
+    
+let id: String
+let produto: String
+let categoria: String
+let preco: Double
+    
+}
